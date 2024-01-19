2f25961852a471dcab80ada50213d36b5030fd36
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-19 07:35:31 -0500

    Update HTML and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "dm-sans": "'DM Sans', sans-serif",
+      },
+      colors: {
+        "green-color": "#27AE60",
+        "gray-color": "#828282",
+        "card-color": "#212121",
+        "color-background": "#171717",
+        "menu-color": "#172554",
+      },
+    },
   },
   plugins: [],
 };
