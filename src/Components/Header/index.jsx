92caf55915af7325a151937f011472f9cb42e495
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-19 12:08:25 -0500

    Add Navbar component and update Header component

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,41 +1,38 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import Navbar from "../Navbar";
 
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="w-full mt-4 h-20">
-      <nav className="w-full h-full flex justify-between items-center">
-        <ul>
-          <li className="max-w-[300px] text-3xl font-semibold ">
-            <Link to="/">Santiago Suarez</Link>
-          </li>
-        </ul>
-        <ul className="z-40">
-          <li
-            className={`w-10 h-10 ${
-              openMenu ? "bg-close-menu" : "bg-open-menu"
-            } bg-cover bg-center bg-green-color rounded-lg cursor-pointer transition-all md:hidden`}
-            onClick={() => {
-              setOpenMenu((state) => !state);
-            }}
-          ></li>
-        </ul>
-        <ul
-          className={`fixed inset-0 bg-menu-color px-[5%] grid gap-6 auto-rows-max content-center justify-items-center clip-circle-0 ${
-            openMenu && "clip-circle-full"
-          } transition-[clip-path] duration-500
-          md:relative md:flex md:gap-6 md:bg-transparent md:clip-circle-full`}
-        >
-          <li>About Me</li>
-          <li>Projects</li>
-          <li>Skills</li>
-          <li>Education</li>
-          <li>Experience</li>
-          <li>Resume</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+    <header>
+      <Navbar />
+      <section className="pt-10 pb-16 md:flex justify-between items-center gap-8">
+        <figure className="md:order-1">
+          <img
+            src="./public/img/foto1.jpg"
+            alt="foto1"
+            className="w-[90%] aspect-square object-cover rounded-full max-w-sm mx-auto"
+          />
+        </figure>
+        <article className="text-center mt-8 md:w-1/2 md:text-left">
+          <h1 className="text-4xl md:text-5xl">Hola, yo soy Santiago Suarez</h1>
+          <p className="text-gray-color text-xl mt-3 mb-6">
+            Full stack developer
+          </p>
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(170px,1fr))] auto-rows-[60px] gap-8 max-w-lg mx-auto md:mx-0">
+            <a
+              href=""
+              className="bg-green-color flex items-center justify-center"
+            >
+              Descargar HdV
+            </a>
+            <a
+              href=""
+              className="flex items-center justify-center border-white border"
+            >
+              Leer mas
+            </a>
+          </div>
+        </article>
+      </section>
     </header>
   );
 };
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <nav className="w-full mt-4 h-20 flex justify-between items-center">
+      <ul>
+        <li className="max-w-[300px] text-3xl font-semibold">
+          <Link to="/">Santiago Suarez</Link>
+        </li>
+      </ul>
+      <ul className="z-40">
+        <li
+          className={`w-10 h-10 ${
+            openMenu ? "bg-close-menu" : "bg-open-menu"
+          } bg-cover bg-center bg-green-color rounded-lg cursor-pointer transition-all md:hidden`}
+          onClick={() => {
+            setOpenMenu((state) => !state);
+          }}
+        ></li>
+      </ul>
+      <ul
+        className={`fixed inset-0 bg-menu-color px-[5%] grid gap-6 auto-rows-max content-center justify-items-center clip-circle-0 ${
+          openMenu && "clip-circle-full"
+        } transition-[clip-path] duration-500
+      md:relative md:flex md:gap-6 md:bg-transparent md:clip-circle-full`}
+      >
+        <li>Acerca de mi</li>
+        <li>Proyectos</li>
+        <li>Habilidades</li>
+        <li>Educacion</li>
+        <li>Experiencia</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
