22da08988ec6922dbd58550bb2d485c3d2465763
santiagoSuarez219 <santiago8628@gmail.com>
2024-01-19 10:02:23 -0500

    Add react-router-dom and create layout component

---
+++ src/Components/Layaout/index.jsx
@@ -0,0 +1,8 @@
+const Layaout = ({ children }) => {
+  return (
+    <div className="w-[90%] mx-auto overflow-hidden max-w-screen-xl">
+      {children}
+    </div>
+  );
+};
+export default Layaout;
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,28 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Layaout from "../../Components/Layaout";
+import Home from "../Home";
+import Menu from "../Menu";
+import NotFound from "../NotFound";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    { path: "/menu", element: <Menu /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+  return routes;
+};
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Layaout>
+        <AppRoutes />
+      </Layaout>
+    </BrowserRouter>
+  );
 };
 export default App;
